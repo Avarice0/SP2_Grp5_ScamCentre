cf6ecd35c3513c7cef59cd87b9739543dc2ca99d
Avarice0 <88842277+Avarice0@users.noreply.github.com>
2022-02-21 13:58:30 +0800

    BlackJack test functions

---
+++ Application/Source/SceneCasinoBJ.cpp
@@ -0,0 +1,410 @@
+#include "SceneCasinoBJ.h"
+#include "SceneGame.h"
+#include <stdlib.h>     /* srand, rand */
+#include <time.h>       /* time */
+#include <iostream>
+
+SceneCasinoBJ::SceneCasinoBJ()
+{
+}
+
+SceneCasinoBJ::~SceneCasinoBJ()
+{
+}
+
+void SceneCasinoBJ::Init()
+{
+	{
+
+		glClearColor(1.f, 1.0f, 1.0f, 0.0f);
+
+		//Enable depth buffer and depth testing
+		glEnable(GL_DEPTH_TEST);
+
+		//Enable back face culling
+		glEnable(GL_CULL_FACE);
+
+		//Default to fill mode
+		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+
+		//enable blending
+		glEnable(GL_BLEND);
+		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	}
+	// Generate a default VAO for now
+	glGenVertexArrays(1, &m_vertexArrayID);
+	glBindVertexArray(m_vertexArrayID);
+	{
+		//Load vertex and fragment shaders
+		m_programID = LoadShaders("Shader//Texture.vertexshader", "Shader//Text.fragmentshader");
+
+		m_parameters[U_MODELVIEW] = glGetUniformLocation(m_programID, "MV");
+		m_parameters[U_MODELVIEW_INVERSE_TRANSPOSE] = glGetUniformLocation(m_programID, "MV_inverse_transpose");
+		m_parameters[U_MATERIAL_AMBIENT] = glGetUniformLocation(m_programID, "material.kAmbient");
+		m_parameters[U_MATERIAL_DIFFUSE] = glGetUniformLocation(m_programID, "material.kDiffuse");
+		m_parameters[U_MATERIAL_SPECULAR] = glGetUniformLocation(m_programID, "material.kSpecular");
+		m_parameters[U_MATERIAL_SHININESS] = glGetUniformLocation(m_programID, "material.kShininess");
+		m_parameters[U_LIGHTENABLED] = glGetUniformLocation(m_programID, "lightEnabled");
+
+		m_parameters[U_NUMLIGHTS] = glGetUniformLocation(m_programID, "numLights");
+		// Get a handle for our "colorTexture" uniform
+		m_parameters[U_COLOR_TEXTURE_ENABLED] = glGetUniformLocation(m_programID, "colorTextureEnabled");
+		m_parameters[U_COLOR_TEXTURE] = glGetUniformLocation(m_programID, "colorTexture");
+		// Get a handle for our "textColor" uniform
+		m_parameters[U_TEXT_ENABLED] = glGetUniformLocation(m_programID, "textEnabled");
+		m_parameters[U_TEXT_COLOR] = glGetUniformLocation(m_programID, "textColor");
+
+		// Get a handle for our "MVP" uniform
+		m_parameters[U_MVP] = glGetUniformLocation(m_programID, "MVP");
+		// Use our shader
+		glUseProgram(m_programID);
+		glUniform1i(m_parameters[U_NUMLIGHTS], 0);
+	}
+	srand(time(NULL));
+	// Init VBO
+	for (int i = 0; i < NUM_GEOMETRY; ++i) {
+		meshList[i] = nullptr;
+	}
+
+	{
+		meshList[GEO_TEXT] = MeshBuilder::GenerateText("text", 16, 16);
+		meshList[GEO_TEXT]->textureID = LoadTGA("Image//calibri.tga");
+		meshList[GEO_MOUSEPOS] = MeshBuilder::GenerateText("mousepos", 16, 16);
+		meshList[GEO_MOUSEPOS]->textureID = LoadTGA("Image//calibri.tga");
+		meshList[GEO_MOUSESTATE] = MeshBuilder::GenerateText("mousestate", 16, 16);
+		meshList[GEO_MOUSESTATE]->textureID = LoadTGA("Image//calibri.tga");
+		
+	}
+
+	Mtx44 projection;
+	projection.SetToPerspective(45.f, 4.f / 3.f, 0.1f, 1000.f);
+	projectionStack.LoadMatrix(projection);
+	//-------------------------------------------------
+
+	Mesh::SetMaterialLoc(m_parameters[U_MATERIAL_AMBIENT], m_parameters[U_MATERIAL_DIFFUSE], m_parameters[U_MATERIAL_SPECULAR], m_parameters[U_MATERIAL_SHININESS]);
+}
+
+void SceneCasinoBJ::Update(double dt)
+{
+	{
+		Application::GetCursorPos(&x, &y);
+		unsigned w = Application::GetWindowWidth();
+		unsigned h = Application::GetWindowHeight();
+		posX = x / w * 80; //convert (0,800) to (0,80)
+		posY = 60 - y / h * 60; //convert (600,0) to (0,60)
+	}
+	static bool bLButtonState = false;
+	if (!bLButtonState && Application::IsMousePressed(0))
+	{
+		bLButtonState = true;
+		mousestate = "LBUTTON DOWN";
+	}
+	else if (bLButtonState && !Application::IsMousePressed(0))
+	{
+		bLButtonState = false;
+		mousestate = "";
+	}
+	static bool bRButtonState = false;
+	if (!bRButtonState && Application::IsMousePressed(1))
+	{
+		bRButtonState = true;
+		mousestate = "RBUTTON DOWN";
+	}
+	else if (bRButtonState && !Application::IsMousePressed(1))
+	{
+		bRButtonState = false;
+		mousestate = "";
+	}
+
+}
+
+void SceneCasinoBJ::Render()
+{
+	{
+		// Render VBO here
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		//Temp variables
+		Mtx44 translate, rotate, scale;
+		Mtx44 MVP;
+
+		//These will be replaced by matrix stack soon
+		Mtx44 model, view, projection;
+		//Set view matrix using camera settings
+	}
+
+
+
+
+
+
+
+	string mousepos = "posX:" + to_string(posX) + ",posY:" + to_string(posY);
+	RenderTextOnScreen(meshList[GEO_MOUSEPOS], mousepos, Color(0.5, 0.5, 1), 2, 0, 2);
+	RenderTextOnScreen(meshList[GEO_MOUSESTATE], mousestate, Color(0.5, 0.5, 1), 2, 0, 3.5);
+	
+	//---------------------------------------------------------
+	Mtx44 mvp = projectionStack.Top() * viewStack.Top() * modelStack.Top();
+}
+
+
+void SceneCasinoBJ::RenderText(Mesh* mesh, std::string text, Color color)
+{
+	if (!mesh || mesh->textureID <= 0) //Proper error check
+		return;
+	glUniform1i(m_parameters[U_TEXT_ENABLED], 1);
+	glUniform3fv(m_parameters[U_TEXT_COLOR], 1, &color.r);
+	glUniform1i(m_parameters[U_LIGHTENABLED], 0);
+	glUniform1i(m_parameters[U_COLOR_TEXTURE_ENABLED], 1);
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, mesh->textureID);
+	glUniform1i(m_parameters[U_COLOR_TEXTURE], 0);
+	for (unsigned i = 0; i < text.length(); ++i)
+	{
+		Mtx44 characterSpacing;
+		characterSpacing.SetToTranslation(i * 1.f, 0, 0); //1.0f is the spacing of each character, you may change this value
+		Mtx44 MVP = projectionStack.Top() * viewStack.Top() * modelStack.Top() * characterSpacing;
+		glUniformMatrix4fv(m_parameters[U_MVP], 1, GL_FALSE, &MVP.a[0]);
+		mesh->Render((unsigned)text[i] * 6, 6);
+	}
+	glBindTexture(GL_TEXTURE_2D, 0);
+	glUniform1i(m_parameters[U_TEXT_ENABLED], 0);
+}
+
+void SceneCasinoBJ::RenderTextOnScreen(Mesh* mesh, std::string text, Color color, float size, float x, float y) {
+	if (!mesh || mesh->textureID <= 0) //Proper error check
+		return;
+	glDisable(GL_DEPTH_TEST); //uncomment for RenderTextOnScreen
+	Mtx44 ortho;
+	ortho.SetToOrtho(0, 80, 0, 60, -10, 10); //size of screen UI
+	projectionStack.PushMatrix();
+	projectionStack.LoadMatrix(ortho);
+	viewStack.PushMatrix(); viewStack.LoadIdentity(); //No need camera for ortho mode
+	modelStack.PushMatrix(); modelStack.LoadIdentity(); //Reset modelStack
+	modelStack.Translate(x, y, 0); modelStack.Scale(size, size, size);
+	glUniform1i(m_parameters[U_TEXT_ENABLED], 1);
+	glUniform3fv(m_parameters[U_TEXT_COLOR], 1, &color.r);
+	glUniform1i(m_parameters[U_LIGHTENABLED], 0);
+	glUniform1i(m_parameters[U_COLOR_TEXTURE_ENABLED], 1);
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, mesh->textureID);
+	glUniform1i(m_parameters[U_COLOR_TEXTURE], 0);
+	for (unsigned i = 0; i < text.length(); ++i)
+	{
+		Mtx44 characterSpacing;
+		characterSpacing.SetToTranslation(0.5f + i * .75f, 0.5f, 0); //1.0f is the spacing of each character, you may change this value
+		Mtx44 MVP = projectionStack.Top() * viewStack.Top() * modelStack.Top() * characterSpacing;
+		glUniformMatrix4fv(m_parameters[U_MVP], 1, GL_FALSE, &MVP.a[0]);
+		mesh->Render((unsigned)text[i] * 6, 6);
+	}
+	glBindTexture(GL_TEXTURE_2D, 0);
+	glUniform1i(m_parameters[U_TEXT_ENABLED], 0);
+	projectionStack.PopMatrix(); viewStack.PopMatrix(); modelStack.PopMatrix();
+	glEnable(GL_DEPTH_TEST); //uncomment for RenderTextOnScreen
+}
+
+void SceneCasinoBJ::Exit()
+{
+	// Cleanup VBO here
+	for (int i = 0; i < NUM_GEOMETRY; ++i)
+	{
+		if (meshList[i])
+		{
+			delete meshList[i];
+		}
+	}
+	glDeleteVertexArrays(1, &m_vertexArrayID);
+	glDeleteProgram(m_programID);
+}
+
+void SceneCasinoBJ::RenderMesh(Mesh* mesh, bool enableLight)
+{
+	Mtx44 MVP, modelView, modelView_inverse_transpose;
+
+	MVP = projectionStack.Top() * viewStack.Top() * modelStack.Top();
+	glUniformMatrix4fv(m_parameters[U_MVP], 1, GL_FALSE, &MVP.a[0]);
+	modelView = viewStack.Top() * modelStack.Top();
+	glUniformMatrix4fv(m_parameters[U_MODELVIEW], 1, GL_FALSE, &modelView.a[0]);
+	if (enableLight)
+	{
+		glUniform1i(m_parameters[U_LIGHTENABLED], 1);
+		modelView_inverse_transpose = modelView.GetInverse().GetTranspose();
+		glUniformMatrix4fv(m_parameters[U_MODELVIEW_INVERSE_TRANSPOSE], 1, GL_FALSE, &modelView_inverse_transpose.a[0]);
+
+		//load material
+		glUniform3fv(m_parameters[U_MATERIAL_AMBIENT], 1, &mesh->material.kAmbient.r);
+		glUniform3fv(m_parameters[U_MATERIAL_DIFFUSE], 1, &mesh->material.kDiffuse.r);
+		glUniform3fv(m_parameters[U_MATERIAL_SPECULAR], 1, &mesh->material.kSpecular.r);
+		glUniform1f(m_parameters[U_MATERIAL_SHININESS], mesh->material.kShininess);
+	}
+	else {
+		glUniform1i(m_parameters[U_LIGHTENABLED], 0);
+	}
+
+	if (mesh->textureID > 0) {
+		glUniform1i(m_parameters[U_COLOR_TEXTURE_ENABLED], 1);
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, mesh->textureID);
+		glUniform1i(m_parameters[U_COLOR_TEXTURE], 0);
+	}
+	else {
+		glUniform1i(m_parameters[U_COLOR_TEXTURE_ENABLED], 0);
+	}
+	mesh->Render(); //this line should only be called once
+	if (mesh->textureID > 0) {
+		glBindTexture(GL_TEXTURE_2D, 0);
+	}
+}
+
+void SceneCasinoBJ::RenderMeshOnScreen(Mesh* mesh, int x, int y, int sizex, int sizey)
+{
+	glDisable(GL_DEPTH_TEST);
+	Mtx44 ortho;
+	ortho.SetToOrtho(0, 80, 0, 60, -10, 10);		//size of screen ui
+	projectionStack.PushMatrix();
+	projectionStack.LoadMatrix(ortho);
+	viewStack.PushMatrix();
+	viewStack.LoadIdentity();		//no need for camera for ortho mode
+	modelStack.PushMatrix();
+	modelStack.LoadIdentity();
+	modelStack.Translate(x, y, 0); modelStack.Scale(sizex, sizey, 0);
+	RenderMesh(mesh, false);
+	projectionStack.PopMatrix();
+	viewStack.PopMatrix();
+	modelStack.PopMatrix();
+	glEnable(GL_DEPTH_TEST);
+}
+
+//#include <iostream>
+//#include <string>
+//#include <conio.h>
+//
+//#include "Deck.h"
+//using namespace std;
+//
+//int main(void) {
+//	int balance = 1000;
+//	int bettingvalue = 0;
+//	int playerinput; bool pass;
+//	Deck OpenDeck;
+//	OpenDeck.resetopen();
+//	while (balance > 0) {
+//		do {
+//			system("CLS");
+//			std::cout << "Welcome to BlackJack!" << std::endl;
+//			std::cout << "Press buttons to change bets." << std::endl;
+//			std::cout << "You have $" << balance << " in your account." << std::endl;
+//			std::cout << "You are betting $" << bettingvalue << std::endl;
+//			std::cout << "Q for -100, W for -50, E for -10, R to start the game, T for +10, Y for +50, U for +100" << std::endl;
+//			pass = false;
+//			playerinput = _getch();
+//			if (playerinput == 114) {		//A to continue next phase
+//				pass = true;
+//			}
+//			else {
+//				//reduce bet value
+//				if (playerinput == 113 && bettingvalue >= 100) {
+//					bettingvalue -= 100;
+//				}
+//				if (playerinput == 119 && bettingvalue >= 50) {
+//					bettingvalue -= 50;
+//				}
+//				if (playerinput == 101 && bettingvalue >= 10) {
+//					bettingvalue -= 10;
+//				}
+//				if (playerinput == 116 && bettingvalue + 10 <= balance) {
+//					bettingvalue += 10;
+//				}
+//				if (playerinput == 121 && bettingvalue + 50 <= balance) {
+//					bettingvalue += 50;
+//				}
+//				if (playerinput == 117 && bettingvalue + 100 <= balance) {
+//					bettingvalue += 100;
+//				}
+//			}
+//		} while (pass == false);
+//
+//		OpenDeck.resethand(OpenDeck.dealerhand); OpenDeck.resethand(OpenDeck.playerhand);
+//		int result = 0;			//0 is no result, 1 is lose, 2 is tie, 3 is win
+//		OpenDeck.addcard(OpenDeck.dealerhand); OpenDeck.addcard(OpenDeck.dealerhand);
+//		OpenDeck.addcard(OpenDeck.playerhand); OpenDeck.addcard(OpenDeck.playerhand);
+//		bool stand = false;
+//		do {
+//			system("CLS");
+//			std::cout << "You have $" << balance << " in your account." << std::endl;
+//			std::cout << "You are betting $" << bettingvalue << std::endl;
+//			std::cout << "dealer hand:" << OpenDeck.valuecount(OpenDeck.dealerhand) << std::endl; OpenDeck.printdeck(OpenDeck.dealerhand);
+//			std::cout << "player hand:" << OpenDeck.valuecount(OpenDeck.playerhand) << std::endl; OpenDeck.printdeck(OpenDeck.playerhand);
+//			if (OpenDeck.valuecount(OpenDeck.playerhand) == 21 && OpenDeck.valuecount(OpenDeck.dealerhand) == 21) {
+//				result = 2;
+//			}
+//			else if (OpenDeck.valuecount(OpenDeck.dealerhand) > 21) {
+//				result = 3;
+//			}
+//			else if (OpenDeck.valuecount(OpenDeck.playerhand) > 21) {
+//				result = 1;
+//			}
+//			else if (stand == true) {
+//				if (OpenDeck.valuecount(OpenDeck.dealerhand) > OpenDeck.valuecount(OpenDeck.playerhand)) {
+//					result = 1;
+//				}
+//				else if (OpenDeck.valuecount(OpenDeck.dealerhand) == OpenDeck.valuecount(OpenDeck.playerhand)) {
+//					result = 2;
+//				}
+//				else if (OpenDeck.valuecount(OpenDeck.dealerhand) < OpenDeck.valuecount(OpenDeck.playerhand)) {
+//					result = 3;
+//				}
+//			}
+//			else if (stand == false) {
+//				if (OpenDeck.valuecount(OpenDeck.dealerhand) <= 21 && OpenDeck.valuecount(OpenDeck.playerhand) <= 21) {
+//					std::cout << "Q to Hit, W to stand" << std::endl;
+//					playerinput = _getch();
+//					if (playerinput == 113) {
+//						OpenDeck.addcard(OpenDeck.playerhand);
+//					}
+//					else if (playerinput == 119) {
+//						while (OpenDeck.valuecount(OpenDeck.dealerhand) <= 17) {
+//							OpenDeck.addcard(OpenDeck.dealerhand);
+//						}
+//						stand = true;
+//					}
+//				}
+//			}
+//
+//		} while (result == 0);
+//
+//		if (result == 1) {
+//			std::cout << "Player lost" << std::endl;
+//			balance -= bettingvalue;
+//		}
+//		else if (result == 2) {
+//			std::cout << "Tie/Push" << std::endl;
+//		}
+//		else if (result == 3) {
+//			std::cout << "Player won" << std::endl;
+//			balance += bettingvalue;
+//		}
+//
+//		std::cout << "Do you want to play again?: Q for yes, W for no";
+//		int playagain = 0;
+//		do {
+//			playerinput = _getch();
+//			if (playerinput == 113) {
+//				playagain = 1;
+//			}
+//			else if (playerinput == 119) {
+//				playagain = 2;
+//			}
+//		} while (playagain == 0);
+//		if (playagain == 2) {
+//			std::cout << "You left with $" << balance << std::endl;
+//			break;
+//		}
+//	}
+//	if (balance <= 0) {
+//		std::cout << "You ran out of money!" << std::endl;
+//	}
+//
+//
+//	return 0;
+//}
---
+++ Application/Source/SceneCasinoBJ.h
@@ -0,0 +1,106 @@
+#ifndef SCENE_CASINO_BJ_H
+#define SCENE_CASINO_BJ_H
+
+#include "Scene.h"
+#include "Mesh.h"
+#include "MatrixStack.h"
+#include "Light.h"
+
+#include "GL\glew.h"
+
+#include "shader.hpp"
+#include "Mtx44.h"
+
+#include "Application.h"
+#include "MeshBuilder.h"
+
+#include "Utility.h"
+
+#include "LoadTGA.h"
+#include "Utility.h"
+#include <iostream>
+
+#include <string>
+#include <stdlib.h>     /* srand, rand */
+#include <time.h>       /* time */
+
+
+using namespace std;
+
+class SceneCasinoBJ : public Scene
+{
+	enum GEOMETRY_TYPE
+	{
+		GEO_AXES,
+		GEO_FLOOR,
+		GEO_QUAD,
+	
+		//text
+		GEO_TEXT,			//debug for text
+		GEO_MOUSEPOS,		//debug for mouse
+		GEO_MOUSESTATE,		//debug for mouse
+
+		NUM_GEOMETRY,
+	};
+	enum UNIFORM_TYPE
+	{
+		U_MVP = 0,
+		U_MODELVIEW,
+		U_MODELVIEW_INVERSE_TRANSPOSE,
+		U_MATERIAL_AMBIENT,
+		U_MATERIAL_DIFFUSE,
+		U_MATERIAL_SPECULAR,
+		U_MATERIAL_SHININESS,
+		U_LIGHTENABLED,
+
+		U_NUMLIGHTS,
+		U_COLOR_TEXTURE_ENABLED,
+		U_COLOR_TEXTURE,
+
+		U_TEXT_ENABLED,
+		U_TEXT_COLOR,
+
+		U_TOTAL,
+	};
+
+public:
+	SceneCasinoBJ();
+	~SceneCasinoBJ();
+
+	virtual void Init();
+	virtual void Update(double dt);
+	virtual void Render();
+	virtual void Exit();
+
+private:
+	unsigned m_vertexArrayID;
+	Mesh* meshList[NUM_GEOMETRY];
+
+	unsigned m_programID;
+	unsigned m_parameters[U_TOTAL];
+
+	MS modelStack, viewStack, projectionStack;
+
+	void RenderMesh(Mesh* mesh, bool enableLight);
+	void RenderMeshOnScreen(Mesh* mesh, int x, int y, int sizex, int sizey);
+
+	void RenderText(Mesh* mesh, std::string text, Color color);
+	void RenderTextOnScreen(Mesh* mesh, std::string text, Color color, float size, float x, float y);
+
+	//mouse input
+	double x, y;
+	float posX, posY;
+	string mousestate;
+
+	string timeprint;
+	int day;
+	int hours=0;
+
+	//time 
+
+	//score/gold earned
+};
+
+
+
+#endif
